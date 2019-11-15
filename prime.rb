81ab3cc17445cc2042d98e34ab16f529caf5a9ce
Luana Faria Lane <luanasfff@gmail.com>
2019-11-15 21:40:28 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@
 def prime?(integer)
 n=2                       #assign divider
 result = true             #create a result variable and make it true by default
-  if inter <= 1             #check if integer less or equal to 1 (i.e. not prime)
+  if integer <= 1             #check if integer less or equal to 1 (i.e. not prime)
     result = false        #save false value into result
   else
     while integer-1>n  do     #start to loop through all possible dividers
